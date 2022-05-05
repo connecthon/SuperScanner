0d576e145a700b431e398a7ebaf5a55cc6424661
Emalude <emanuele.ct91@gmail.com>
2022-05-05 19:38:29 +0100

    PCA

--- Emanuele/eval.py
+++ Emanuele/eval.py
@@ -2,10 +2,12 @@ import net
 from scipy.io import savemat
 import numpy as np
 
-_, _, valid_data = net.load_data_voxels('./data/sub-06400_ndwi.mat', 139, 140, num_y_cols=1, batch_size=50,  device='cpu')
+_, _, valid_data = net.load_data_voxels('./data/sub-06400_ndwi.mat', 139, 140, num_y_cols=1, batch_size=50,  pca_var=0.9, device='cpu')
 
-trained_model = net.voxelnet(139,100,1).to('cpu')
-prediction = net.evaluate_plot(trained_model, './models/voxelnet', valid_data)
+in_dim = valid_data.dataset[:][0].shape[1]
+print(in_dim)
+trained_model = net.voxelnet(in_dim,10,1).to('cpu')
+predictions = net.evaluate_plot(trained_model, './models/voxelnet_pca', valid_data)
 
 savemat('./data/prediction.mat', {'predictions':predictions})
 np.savetxt('./data/prediction.csv', predictions, delimiter=',')
--- Emanuele/train.py
+++ Emanuele/train.py
@@ -8,10 +8,10 @@ start = time.time()
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-train_dl, test_dl, _ = net.load_data_voxels('./data/sub-06400_ndwi.mat', 139, 139, num_y_cols=1, batch_size=128,  pca_var=None, device=device)
+train_dl, test_dl, _ = net.load_data_voxels('./data/sub-06400_ndwi.mat', 139, 139, num_y_cols=1, batch_size=128,  pca_var=0.90, device=device)
 
 in_dim = train_dl.dataset[:][0].shape[1]
-model = net.voxelnet(in_dim, 100, 1).to(device)
+model = net.voxelnet(in_dim, 10, 1).to(device)
 opt = torch.optim.Adam(model.parameters(), lr=1e-4)
 
 trained_model = net.train(model, train_dl, test_dl, opt, num_epochs=5)
