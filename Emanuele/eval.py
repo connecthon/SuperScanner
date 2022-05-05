3809cd6169f76f8a9dce5e96e62b6397f6aaf66e
pall75 <pedro.luque.laguna@gmail.com>
2022-05-05 19:23:47 +0100

    adding number of hidden dimensions

--- Emanuele/eval.py
+++ Emanuele/eval.py
@@ -7,7 +7,8 @@ Y=265
 
 _, _, valid_data = net.load_data_voxels('./data/sub-06400_ndwi.mat', X, Y, num_y_cols=1, batch_size=50,  device='cpu')
 
-trained_model = net.voxelnet(len(X),100,1).to('cpu')
+num_hidden_dims=10
+trained_model = net.voxelnet(len(X),num_hidden_dims,1).to('cpu')
 predictions = net.evaluate_plot(trained_model, './models/voxelnet_' + str(Y), valid_data)
 
 savemat('./data/prediction' + str(Y) +'.mat', {'predictions':predictions})
